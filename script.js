2c6ebba467742f78d36ac0f12a6c7ef4482328e9
Anay Sharma <anaysharma8791@gmail.com>
2022-08-02 18:49:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const API_KEY = "81987dd79e0d74f1918035c9e09b452e";
+
+async function getLocation ( city ) {
+	try {
+		const location = await fetch (
+			`http://api.openweathermap.org/geo/1.0/direct?q=${ city }&limit=5&appid=${ API_KEY }`
+		);
+		const locationData = await location.json();
+
+	} catch ( error ) {
+		console.error( error );
+	}
+}
+
+function getCurrentLocation () {
+	navigator.geolocation.getCurrentPosition (
+		onSuccess,
+		onError,
+		{
+			maximumAge:60000,
+			timeout:10000,
+			enableHighAccuracy:true
+		}
+	);
+	function onSuccess ( position ) {
+		const {
+			latitude,
+			longitude
+		} = position.coords;
+		return { latitude, longitude };
+	}
+	function onError ( error ) {
+		console.log ( error );
+	}
+}
+
+async function getWeather ( coordinates ) {
+	try {
+		let latitude = coordinates.latitude;
+		let longitude = coordinates.longitude;
+		const weatherResponse = await fetch (
+			`https://api.openweathermap.org/data/2.5/weather?lat=${ latitude }&lon=${ longitude }&appid=${ API_KEY }`
+		);
+		const weatherData = await weatherResponse.json();
+		console.log( weatherData );
+	} catch ( error ) {
+		console.error( error );						
+	}
+}
+
+let dat = getLocation("london");
+console.log(dat)
