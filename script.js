fdc1dd03a3ef17bdde7ea4847f8fbf743c5e4aa6
Anay Sharma <anaysharma8791@gmail.com>
2022-08-04 10:31:47 +0530

    added city list dialog

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+const body = document.body;
+const mainBox = document.createElement("main");
+const weatherBox = document.createElement("div");
+const weatherInfo = document.createElement("div");
+const searchBox = document.createElement("div");
+const searchInput = document.createElement("input");
+const searchButton = document.createElement("button");
+const currentLocationButton = document.createElement("button");
+const messageBox = document.createElement("div");
+const weekWeatherBox = document.createElement("div");
+const modalBox = document.createElement("dialog");
+const closeModal = document.createElement("button");
+const submitModal = document.createElement("button");
+
+closeModal.classList.add("close-modal");
+submitModal.classList.add("submit-modal");
+weatherBox.classList.add("weather-box");
+weatherInfo.classList.add("weather-info");
+searchBox.classList.add("search-box");
+searchButton.classList.add("search-button");
+currentLocationButton.classList.add("location-button");
+messageBox.classList.add("message-box");
+weekWeatherBox.classList.add("week-weather");
+
+closeModal.innerText = "✖";
+submitModal.innerText = "Submit";
+searchButton.innerText = "Search";
+currentLocationButton.innerText = "Use Current Location";
+searchInput.setAttribute("type", "text");
+
+searchBox.append (
+	searchInput,
+	messageBox,
+	searchButton,
+	currentLocationButton
+);
+
+weatherBox.appendChild( weatherInfo );
+
+mainBox.append (
+	weatherBox,
+	searchBox,
+	weekWeatherBox
+);
+
+body.append( mainBox, modalBox );
+
 const API_KEY = "81987dd79e0d74f1918035c9e09b452e";
 
 async function getLocation ( city ) {
@@ -55,7 +102,8 @@ function parseResponseJson ( data ) {
 		let lon = data.lon;
 		return { lat, lon };
 	} else {
-
+		const requiredData = {};
+		// requiredData.
 	}
 }
 getLocation("london")
